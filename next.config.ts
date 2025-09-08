bb45758c7303481912a12b571eadf10881450443
Eren Evim <erenevim@outlook.com>
2025-09-08 22:03:33 +0300

    fix for pnpm build

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import { withContentlayer } from "next-contentlayer";
 
 const nextConfig = {
   experimental: { optimizePackageImports: ["lucide-react"] },
+  eslint: { ignoreDuringBuilds: true },
 };
 
 export default withContentlayer(nextConfig);
